2096eb06a5e8616f0aff18e2f16552f07f34c023
Piotr Majkowski <piotr.majkowski@gmail.com>
2018-03-03 13:23:02 +0100

    add slide effect

--- js/scrollUp.js
+++ js/scrollUp.js
@@ -2,9 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.documentElement.scrollTop > 100) {
+        $('button.btn-scroll').slideDown('slow') ;
         document.getElementById("scroll-up").style.display = "block";
     } else {
-        document.getElementById("scroll-up").style.display = "none";
+        $('button.btn-scroll').hide('slow') ;
+        // document.getElementById("scroll-up").style.display = "none";
     }
 }
 
