63d1a19af8a4fd7755e7787e08e9ca8d2ab37fd3
Piotr Majkowski <piotr.majkowski@gmail.com>
2018-03-13 23:11:07 +0100

    add folder motion in 3 different screen resolution

--- js/game.js
+++ js/game.js
@@ -1,14 +1,13 @@
 var positionFolder;
+var screenWidth = window.innerWidth;
 
 
 function init()
 {
-    document.getElementById('music-folder').style.left ='1%';
+    document.getElementById('music-folder').style.left ='0';
     positionFolder = parseInt(document.getElementById('music-folder').style.left);
 
 }
-
-
 init();
 
 const folderMove = (e) =>
@@ -16,13 +15,19 @@ const folderMove = (e) =>
     switch (e.keyCode)
     {
         case 37:
-            if (positionFolder>0){
+            if (positionFolder>0) {
                 document.getElementById('music-folder').style.left = (positionFolder - 2).toString() + 'px';
                 positionFolder-=2;
             }
             break;
         case 39:
-            if (positionFolder<182){
+            if (positionFolder<182 && screenWidth<768){
+                document.getElementById('music-folder').style.left = (positionFolder + 2).toString() + 'px';
+                positionFolder+=2;
+            } else if (positionFolder<542 && screenWidth>768){
+                document.getElementById('music-folder').style.left = (positionFolder + 2).toString() + 'px';
+                positionFolder+=2;
+            } else if (positionFolder<842 && screenWidth>1170){
                 document.getElementById('music-folder').style.left = (positionFolder + 2).toString() + 'px';
                 positionFolder+=2;
             }
