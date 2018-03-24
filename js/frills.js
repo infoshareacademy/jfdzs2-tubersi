bece22c075d9b76634f66f1acdb867939fb8977b
Dorota Pawłowska <dpawlowska@op.pl>
2018-03-24 11:50:50 +0100

    removed script js for carousel transformation
    instead added one @media for div.wraper

--- js/frills.js
+++
@@ -1,8 +0,0 @@
-// ##### DOROTA - Rotacja zawartości w zajawce ######
-
-$( window ).load(function() {
-    if($(window).width() >=1200)
-        $('.wraper').addClass("container");
-    else
-        $('.wraper').removeClass("container");
-});
