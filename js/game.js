5a3348fc5f8655b5fb840bd71063a6f393dc9cba
Piotr Majkowski <piotr.majkowski@gmail.com>
2018-04-06 12:19:06 +0200

    add css style for game and cleanings in game.js

--- js/game.js
+++ js/game.js
@@ -1,32 +1,29 @@
 // START GAME
 //
 
+
+
+//choic of level and start
 var level1 = document.getElementById("level-1");
 var level2 = document.getElementById("level-2");
 var time = document.getElementById('time-to-end');
 var sek = 60;
 
 level1.addEventListener("click", init);
-level1.addEventListener("click", timeStart);
 level2.addEventListener("click", init);
-level2.addEventListener("click", timeStart);
 
 
+
+// function start game time
 function timeStart(){
     if(sek>0) {
         time.innerHTML='<b>'+sek+'</b>'+' sekund';
         setTimeout(function(){timeStart(time,--sek)},1000)
     } else {
         time.innerHTML='Koniec czasu';
+
     }
 }
-
-
-
-var positionFolder;
-var positionTune;
-var screenWidth = window.innerWidth;
-
 
 // function sets the star position of the folder for different resolutions
 function startPositionFolder (){
@@ -39,72 +36,71 @@ function startPositionFolder (){
     }
 }
 
+var positionFolder;
+var positionTune;
+var screenWidth = window.innerWidth;
 
 function init() {
     document.getElementById('select-level').style.display='none';
-
     startPositionFolder();
-
+    timeStart();
 
     positionFolder = parseInt(document.getElementById('music-folder').style.left);
-
-    //pozycja startowa nuta
-    document.getElementById('tune-1').style.top = '90';
     positionTune = parseInt(document.getElementById('tune-1').style.top);
     setInterval(moveTunes,60);
-    setTimeout(activeTune,500);
-    setTimeout(activeTune1,800);
+    setTimeout(activeTune2,500);
+    setTimeout(activeTune3,800);
 
-
-
-    var element1 = document.getElementById("tune-1");
-    var element2 = document.getElementById("tune-2");
-    var element3 = document.getElementById("tune-3");
-
-    var posY1 = 90, posY2 = 90, posY3 = 90;
-
-    var activeTunes1 = true;
-    var activeTunes2 = false;
-    var activeTunes3 = false;
-
-    function activeTune(){
-        activeTunes2 = true;
-    }
-
-    function activeTune1(){
-        activeTunes3 = true;
-    }
-
-    function moveTunes() {
-
-        if(activeTunes1 === true){
-            posY1+=10;
-            element1.style.top = posY1+'px' ;
-            if(posY1 > 390){
-                posY1 = 90;
-            }
-        }
-        if(activeTunes2 === true){
-            posY2+=5;
-            element2.style.top = posY2+'px' ;
-            if(posY2 > 390){
-                posY2 = 90;
-            }
-        }
-        if(activeTunes3 === true){
-            posY3+=15;
-            element3.style.top = posY3+'px' ;
-            if(posY3 > 390){
-                posY3 = 90;
-            }
-        }
-    }
 
 }
 
 
+//SPADAJĄCE NUTY
+var activeTunes1 = true;
+var activeTunes2 = false;
+var activeTunes3 = false;
+
+function activeTune2(){
+    activeTunes2 = true;
+}
+
+function activeTune3(){
+    activeTunes3 = true;
+}
+
+var posY1 = 90, posY2 = 90, posY3 = 90;
+
+function moveTunes() {
+    var element1 = document.getElementById("tune-1");
+    var element2 = document.getElementById("tune-2");
+    var element3 = document.getElementById("tune-3");
 
 
+    if(activeTunes1 === true){
+        posY1+=10;
+        element1.style.top = posY1+'px' ;
+        if(posY1 > 390){
+            posY1 = 90;
+        }
+    }
+    if(activeTunes2 === true){
+        posY2+=5;
+        element2.style.top = posY2+'px' ;
+        if(posY2 > 390){
+            posY2 = 90;
+        }
+    }
+    if(activeTunes3 === true){
+        posY3+=15;
+        element3.style.top = posY3+'px' ;
+        if(posY3 > 390){
+            posY3 = 90;
+        }
+    }
+}
+
+
+//folder move by key
 const folderMove = (e) =>
 {
     switch (e.keyCode)
@@ -135,8 +131,6 @@ const folderMove = (e) =>
             break;
     }
 }
-
-
 
 document.addEventListener('keydown',folderMove);
 
