98a89ea4022e882e268c5ba86f80278c006e4e11
Piotr Majkowski <piotr.majkowski@gmail.com>
2018-04-13 19:56:47 +0200

    feature-cookies ready

--- js/frills.js
+++ js/frills.js
@@ -3,4 +3,14 @@
 function closeCookies() {
     var cookiesDisplay = document.querySelector('.cookies');
     cookiesDisplay.style.display= 'none';
+    document.cookie = "tubers";
 }
+
+function removeCookiePopup() {
+    if(document.cookie === "tubers"){
+        var cookiesDisplay = document.querySelector('.cookies');
+        cookiesDisplay.style.display= 'none';
+    }
+}
+
+removeCookiePopup();
