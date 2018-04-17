d27aec372c4ebd5c0a056878a2e8928d4024bf86
Piotr Majkowski <piotr.majkowski@gmail.com>
2018-04-17 21:54:35 +0200

    checkCollision for three resolituons

--- js/game.js
+++ js/game.js
@@ -10,6 +10,7 @@ var score;
 var positionFolder;
 var screenWidth = window.innerWidth;
 
+
 level1.addEventListener("click", init);
 level2.addEventListener("click", init);
 
@@ -40,43 +41,104 @@ function addPoint(){
     }
 }
 
+
+//checkCollision for three other resolutions
 function checkCollision(){
     var positionFolder = parseInt(document.getElementById('music-folder').style.left);
     var positionTune1 = parseInt(document.getElementById('tune-1').style.top);
     var positionTune2 = parseInt(document.getElementById('tune-2').style.top);
     var positionTune3 = parseInt(document.getElementById('tune-3').style.top);
+    var screenWidthMobile = screenWidth<768;
+    var screenWidthTablet = screenWidth>768 && screenWidth<1170;
+    var screenWidthDesktop = screenWidth>1170;
 
-    //when tune put in to folder
-
-    if (positionFolder===121 && positionTune1===360){
+    //when tune put in to folder for DESKTOP
+    if (positionFolder===121 && positionTune1===360 && screenWidthDesktop){
         posYtune1 = 90;
-        $('#thumb-up-1').fadeIn('fast').fadeOut('slow');  // trzebe zamienić na JS
+        $('#thumb-up-1').fadeIn('fast').fadeOut('slow');
         return true;
-    } else if (positionFolder===426 && positionTune2===360){
+    } else if (positionFolder===426 && positionTune2===360 && screenWidthDesktop){
         posYtune2 = 90;
         $('#thumb-up-2').fadeIn('fast').fadeOut('slow');
         return true;
-    } else if (positionFolder===731 && positionTune3===360){
+    } else if (positionFolder===731 && positionTune3===360 && screenWidthDesktop){
+        posYtune3 = 90;
+        $('#thumb-up-3').fadeIn('fast').fadeOut('slow');
+        return true;
+    }
+    //when tune put in to folder for TABLET
+    if (positionFolder===55 && positionTune1===360 && screenWidthTablet){
+        posYtune1 = 90;
+        $('#thumb-up-1').fadeIn('fast').fadeOut('slow');
+        return true;
+    } else if (positionFolder===260 && positionTune2===360 && screenWidthTablet) {
+        posYtune2 = 90;
+        $('#thumb-up-2').fadeIn('fast').fadeOut('slow');
+        return true;
+    } else if (positionFolder===465 && positionTune3===360 && screenWidthTablet){
+        posYtune3 = 90;
+        $('#thumb-up-3').fadeIn('fast').fadeOut('slow');
+        return true;
+    }
+    //when tune put in to folder for MOBILE
+    if (positionFolder===20 && positionTune1===360 && screenWidthMobile){
+        posYtune1 = 90;
+        $('#thumb-up-1').fadeIn('fast').fadeOut('slow');
+        return true;
+    } else if (positionFolder===96 && positionTune2===360 && screenWidthMobile){
+        posYtune2 = 90;
+        $('#thumb-up-2').fadeIn('fast').fadeOut('slow');
+        return true;
+    } else if (positionFolder===172 && positionTune3===360 && screenWidthMobile) {
         posYtune3 = 90;
         $('#thumb-up-3').fadeIn('fast').fadeOut('slow');
         return true;
     }
 
-    //when tune crash with floor
-
-    if (positionTune1===390){
+    //when tune crash with floor DESKTOP
+    if (positionTune1===390 && screenWidth>1170){
         posYtune1 = 90;
         $('#thumb-down-1').fadeIn('fast').fadeOut('slow');  // trzebe zamienić na JS
         return false;
-    } else if (positionTune2 ===390){
+    } else if (positionTune2 ===390 && screenWidth>1170){
         posYtune2 = 90;
         $('#thumb-down-2').fadeIn('fast').fadeOut('slow');
         return false;
-    } else if (positionTune3 === 390){
+    } else if (positionTune3 === 390 && screenWidth>1170){
         posYtune3 = 90;
         $('#thumb-down-3').fadeIn('fast').fadeOut('slow');
         return false;
     }
+    //when tune crash with floor TABLET
+    if (positionTune1===390 && screenWidthTablet){
+        posYtune1 = 90;
+        $('#thumb-down-1').fadeIn('fast').fadeOut('slow');  // trzebe zamienić na JS
+        return false;
+    } else if (positionTune2 ===390 && screenWidthTablet){
+        posYtune2 = 90;
+        $('#thumb-down-2').fadeIn('fast').fadeOut('slow');
+        return false;
+    } else if (positionTune3 === 390 && screenWidthTablet){
+        posYtune3 = 90;
+        $('#thumb-down-3').fadeIn('fast').fadeOut('slow');
+        return false;
+    }
+    //when tune crash with floor MOBILE
+    if (positionTune1===390 && screenWidthMobile){
+        posYtune1 = 90;
+        $('#thumb-down-1').fadeIn('fast').fadeOut('slow');  // trzebe zamienić na JS
+        return false;
+    } else if (positionTune2 ===390 && screenWidthMobile){
+        posYtune2 = 90;
+        $('#thumb-down-2').fadeIn('fast').fadeOut('slow');
+        return false;
+    } else if (positionTune3 === 390 && screenWidthMobile){
+        posYtune3 = 90;
+        $('#thumb-down-3').fadeIn('fast').fadeOut('slow');
+        return false;
+    }
+
+
 }
 
 // function sets the star position of the folder for different resolutions
@@ -119,35 +181,35 @@ function activeTune3(){
 }
 
 function moveTunes() {
-    var element1 = document.getElementById("tune-1");
-    var element2 = document.getElementById("tune-2");
-    var element3 = document.getElementById("tune-3");
-
     addPoint();
+    var tune1 = document.getElementById("tune-1");
+    var tune2 = document.getElementById("tune-2");
+    var tune3 = document.getElementById("tune-3");
 
     if(activeTunes1 === true){
         posYtune1+=10;
-        element1.style.top = posYtune1+'px' ;
+        tune1.style.top = posYtune1+'px' ;
         if(posYtune1 > 390){
             posYtune1 = 90;
         }
     }
     if(activeTunes2 === true){
         posYtune2+=5;
-        element2.style.top = posYtune2+'px' ;
+        tune2.style.top = posYtune2+'px' ;
         if(posYtune2 > 390){
             posYtune2 = 90;
         }
     }
     if(activeTunes3 === true){
         posYtune3+=15;
-        element3.style.top = posYtune3+'px' ;
+        tune3.style.top = posYtune3+'px' ;
         if(posYtune3 > 390){
             posYtune3 = 90;
         }
     }
 }
 
+//move folder desktops
 const folderMove = (e) =>
 {
     switch (e.keyCode)
@@ -178,7 +240,31 @@ const folderMove = (e) =>
             break;
     }
 }
-
 document.addEventListener('keydown',folderMove);
 
 
+// //touchmove for mobile.
+//
+// var musicFolder = document.getElementById('music-folder');
+// musicFolder.addEventListener('touchmove', function(e){
+//    var
+//     e.preventDefault();
+// });
+//
+//
+// //Set .touchmove() listener for basket for mobile devices
+// $('.basket').bind('touchmove', function (e) {
+//     e.preventDefault();
+//     var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+//     var newX = Math.floor(env.calcPct(touch.pageX) - basket.width / 2 - env.calcPct($('.game-board').offset().left));
+//     if ((newX + basket.width < env.maxPosX) && (newX > env.minPosX)) {
+//         basket.positionX = newX;
+//     } else if (newX + basket.width >= env.maxPosX) {
+//         basket.positionX = env.maxPosX - basket.width;
+//     } else {
+//         basket.positionX = env.minPosX;
+//     }
+//     basket.moveBasket(basket.positionX);
+// });
+//
+//
