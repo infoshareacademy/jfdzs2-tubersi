beb5bcdc667f8748ee43e93fffb95395a0775889
Dorota Pawłowska <dpawlowska@op.pl>
2018-04-22 22:00:35 +0200

    itty-bitty fixes to syntax in html and frills

--- js/frills.js
+++ js/frills.js
@@ -22,12 +22,9 @@ $(document).ready(function(){
 
         if (this.hash !== "") {
             event.preventDefault();
-
             var hash = this.hash;
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
+            $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
                 window.location.hash = hash;
             });
         }
